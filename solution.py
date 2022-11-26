d7b6727c0bc1c9a4ff8a32b06297270ec56d57e5
yunah <dbsdk7865@inha.edu>
2022-11-27 05:49:29 +0900

    제출 파일

--- solution.py
+++ solution.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 #PLEASE WRITE THE GITHUB URL BELOW!
 #https://github.com/yunah7/OSS_Project2.git
 
